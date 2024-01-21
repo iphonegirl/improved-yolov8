3c58074eb6fc988cc41ff0704545418337434d52
zhangyanwei <zhangyanwei@eccom,com,cn>
2024-01-21 23:58:08 +0800

    modify training bash

--- costom/scripts/batch_train.sh
+++ costom/scripts/batch_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-sh ./costom/scripts/train.sh /home/zhangyanwei/experiments/coco8/runs/yolov8/train/yolov8n \
+sh ./costom/scripts/train.sh ./runs/yolov8/train/yolov8n \
                       ./costom/config/coco-training-config.yaml \
                       ./costom/models/yolov8n.yaml
